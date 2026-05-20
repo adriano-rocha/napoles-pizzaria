d4b159ffbd0023fffe0b8a349fe5cac8eccf23ad
Adriano Rocha <adrianorocha.dev@gmail.com>
2026-05-20 16:26:52 -0300

    fix: busca produtos direto no prisma sem axios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,11 @@ import { PizzaList } from "@/components/home/pizza-list";
 import { Header } from "@/components/layout/header";
 import About from "@/components/home/about";
 import Footer from "@/components/layout/footer";
-import { api } from "@/lib/axios";
+import { prisma } from "@/lib/prisma";
 
 export default async function Page() {
-  const pizzaReq = await api.get("/pizzas");
-  const pizzas = pizzaReq.data.pizzas ?? [];
-  
+  const pizzas = await prisma.product.findMany();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
